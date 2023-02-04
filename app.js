417316d4ba5192775d60ab74b8da46ff74baa8b6
Sunny Diwakar <sunny12jun@gmail.com>
2023-02-04 17:21:48 +0530

    app .js and miniml setup for React

---
+++ app.js
@@ -0,0 +1,19 @@
+const headingh1 = React.createElement(
+  "h1",
+  {
+    id: "heading1",
+  },
+  "I am Heading H1"
+);
+const headingh2 = React.createElement(
+  "h2",
+  {
+    id: "heading2",
+  },
+  "I am Heading H2"
+);
+
+const container = React.createElement("div", {}, [headingh1, headingh2]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(container);
