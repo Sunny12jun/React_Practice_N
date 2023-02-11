9d2e3ab34f38b5d04e8fc7b301bd3d4982fb3b2b
Sunny Diwakar <sunny12jun@gmail.com>
2023-02-11 13:18:06 +0530

    Assignmnets and chapter 2

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 const headingh1 = React.createElement(
   "h1",
   {
     id: "heading1",
+    key: "h1",
   },
   "I am Heading H1"
 );
@@ -11,10 +12,13 @@ const headingh2 = React.createElement(
   "h2",
   {
     id: "heading2",
+    key: "h2",
   },
-  "I am Heading H2"
+  "I am Heading H2 "
 );
-
+/**
+ * di
+ */
 const container = React.createElement("div", { id: "container" }, [
   headingh1,
   headingh2,
